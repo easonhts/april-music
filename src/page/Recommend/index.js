cbc9bf502805ec57b1b5a92f2e9db853fcdfbfad
wukeqiang <wukeqiang@jiayouxueba.cn>
2020-11-20 17:27:33 +0800

    feat:添加首页轮播图组件

--- src/page/Recommend/index.js
+++ src/page/Recommend/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Slider from '../../components/slider/index';
 
 function Recommend(props) {
+  const bannerList = [1,2,3,4].map(item => {
+    return { url: 'http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg'}
+  })
   return (
-    <div>
-      Recommend
-    </div>
+    <>
+      <Slider bannerList={bannerList} />
+    </>
   )
 }
 
